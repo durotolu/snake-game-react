dd626ef852d53874a4e91865340616b337d83bce
Modurotolu Olokode <durolawk@gmail.com>
2019-12-02 23:28:03 +0100

    added directions onkeydown

--- snake-game/src/App.js
+++ snake-game/src/App.js
@@ -6,8 +6,8 @@ import './App.css';
 const getRandomCoordinates = () => {
   let min = 1;
   let max = 98;
-  let x = Math.floor((Math.random()*(max-min+1)+min)/2)*2;
-  let y = Math.floor((Math.random()*(max-min+1)+min)/2)*2;
+  let x = Math.floor((Math.random() * (max - min + 1) + min) / 2) * 2;
+  let y = Math.floor((Math.random() * (max - min + 1) + min) / 2) * 2;
 
   return [x, y]
 }
@@ -17,6 +17,29 @@ function App() {
     [2, 0]
   ]);
   const [food, setFood] = useState(getRandomCoordinates())
+  const [direction, setDirection] = useState('RIGHT')
+
+  useEffect(() => {
+    document.onkeydown = onKeyDown
+  }, [])
+
+  const onKeyDown = (e) => {
+    e = e || window.event;
+    switch (e.keyCode) {
+      case 38:
+        setDirection('UP');
+        break;
+      case 40:
+        setDirection('DOWN');
+        break;
+      case 37:
+        setDirection('LEFT');
+        break;
+      case 39:
+        setDirection('RIGHT');
+        break;
+    }
+  }
 
   return (
     <div className="game-area">
