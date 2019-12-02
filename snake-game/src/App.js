c1e3924385715ac68b1c64a6e3a6d34466701261
Modurotolu Olokode <durolawk@gmail.com>
2019-12-02 18:58:55 +0100

    created Food component

--- snake-game/src/App.js
+++ snake-game/src/App.js
@@ -1,17 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import Snake from './Snake'
-// import Food from './Food'
+import Food from './Food'
 import './App.css';
 
 function App() {
   const [snakeDots, setSnakeDots] = useState([
     [0, 0],
     [2, 0]
-  ])
+  ]);
+  const [food, setFood] = useState([6, 8])
 
   return (
     <div className="game-area">
       <Snake snakeDots={snakeDots} />
+      <Food dot={food} />
     </div>
   );
 }
