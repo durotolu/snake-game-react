f784ff5d35e3aae5492a649a271076e435ba60f9
Modurotolu Olokode <durolawk@gmail.com>
2019-12-02 18:31:47 +0100

    rendered snake dots to game area

--- snake-game/src/App.js
+++ snake-game/src/App.js
@@ -7,6 +7,9 @@ function App() {
 
   return (
     <div className="game-area">
+      <div className="snake-dot" style={{top:0, left:0}}></div>
+      <div className="snake-dot" style={{top:0, left:'2%'}}></div>
+      <div className="snake-dot" style={{top:0, left:'4%'}}></div>
     </div>
   );
 }
