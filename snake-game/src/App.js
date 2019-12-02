09c8048233c942bdc1dc8ec05fe3a483fe3f6da3
Modurotolu Olokode <durolawk@gmail.com>
2019-12-02 19:44:17 +0100

    replaced food state with random dynamic coordinates

--- snake-game/src/App.js
+++ snake-game/src/App.js
@@ -3,12 +3,20 @@ import Snake from './Snake'
 import Food from './Food'
 import './App.css';
 
+const getRandomCoordinates = () => {
+  let min = 1;
+  let max = 98;
+  let x = Math.floor((Math.random()*(max-min+1)+min)/2)*2;
+  let y = Math.floor((Math.random()*(max-min+1)+min)/2)*2;
+
+  return [x, y]
+}
 function App() {
   const [snakeDots, setSnakeDots] = useState([
     [0, 0],
     [2, 0]
   ]);
-  const [food, setFood] = useState([6, 8])
+  const [food, setFood] = useState(getRandomCoordinates())
 
   return (
     <div className="game-area">
